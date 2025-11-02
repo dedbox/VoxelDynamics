2c58a47b49cd7f76a4d76328888fba6bcdb74281
Eric Griffis <dedbox@gmail.com>
2025-11-01 20:06:38 -0700

    update client API exports

--- VoxelDynamics/src/VoxelDynamics.hpp
+++ VoxelDynamics/src/VoxelDynamics.hpp
@@ -9,6 +9,9 @@
 #include <VoxelDynamics/Core/Window.hpp>
 
 #include <VoxelDynamics/Vulkan/Context.hpp>
+#include <VoxelDynamics/Vulkan/Device.hpp>
+#include <VoxelDynamics/Vulkan/PhysicalDevice.hpp>
 #include <VoxelDynamics/Vulkan/PipelineConfig.hpp>
 #include <VoxelDynamics/Vulkan/PipelineManager.hpp>
+#include <VoxelDynamics/Vulkan/RenderQueue.hpp>
 #include <VoxelDynamics/Vulkan/Renderer.hpp>
