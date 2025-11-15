fc269354b037bded771b3cffecc21c831876639e
Eric Griffis <dedbox@gmail.com>
2025-11-14 22:02:00 -0800

    minor cleanup

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -4,7 +4,6 @@
 #include "glm/ext/vector_float2.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/gtc/matrix_transform.hpp"
-// #include "glm/trigonometric.hpp"
 
 // Sandbox App /////////////////////////////////////////////////////////////////////////////////////
 
